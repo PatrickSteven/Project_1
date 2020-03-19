c0f70d6e541f07d54f0eeea32e1f788925e2f1f1
PatrickSteven <patrick@estudiantec.cr>
2020-03-18 22:29:14 -0600

    Proyecto Visual Studio

--- Project_1/Controllers/HomeController.cs
+++ Project_1/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Project_1.Controllers
 {
@@ -10,7 +14,39 @@ namespace Project_1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_DB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "dbo.SPI_Propiedad";
+                cmd.Parameters.Add("@numeroFinca", SqlDbType.Int).Value = 5254;
+                cmd.Parameters.Add("@valor", SqlDbType.Int).Value = 10000;
+                cmd.Parameters.Add("@Direccion", SqlDbType.VarChar).Value = "La Berga";
+                cmd.Connection = connection;
+                
+                String message;
+                
+                try
+                {
+                    connection.Open();
+                    object result = cmd.ExecuteScalar();
+                    message = "Dato Insertado" + result.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                    Console.WriteLine(ex);
+                    throw;
+
+                }
+                finally
+                {
+                    connection.Close();
+                    
+                }
+                return Content(message);
+            }
         }
 
         public ActionResult About()
