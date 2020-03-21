3b51f0c5066902e6378f3364c7d31d5fb9a576e4
alegarciy <ale.garciy@gmail.com>
2020-03-21 13:33:17 -0600

    Conexiones
    Comprobante de Pago

---
+++ Project_1/Models/Comprobante_de_Pago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project_1.Models
+{
+    public class Comprobante_de_Pago
+    {
+        [DataType(DataType.DateTime)]
+        public int fecha { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Total must be numeric")]
+        public int total { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "idPropiedad must be numeric")]
+        public int idPropiedad { get; set; }
+    }           
+}
--- Project_1/Models/Propiedad.cs
+++ Project_1/Models/Propiedad.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+// imports
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Project_1.Models
 {
     public class Propiedad
     {
+
+        [RegularExpressionAttribute("^[0-9]*$", ErrorMessage = "numeroFinca must be numeric")]
         public int numeroFinca { get; set; }
+
+        [RegularExpressionAttribute("^[0-9]*$", ErrorMessage = "Valor must be numeric")]
         public int valor { get; set; }
+
+
         public String direccion { get; set; }
 
         Propiedad(int numeroFinca, int valor, String direccion)
