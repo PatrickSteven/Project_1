4576c87539d16d7e72cd523556a535a8dfeb375c
PatrickSteven <patrick@estudiantec.cr>
2020-03-27 17:47:39 -0600

    Usuario Select and Insert

--- Project_1/Controllers/UsuarioController.cs
+++ Project_1/Controllers/UsuarioController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project_1.Models;
 using Project_1.Models.Authentication;
+using Project_1.ViewModels;
 
 namespace Project_1.Controllers
 {
@@ -12,7 +14,47 @@ namespace Project_1.Controllers
     {
         // GET: Usuario
         public ActionResult Index()
-        {   
+        {
+            var usuarios = new UsuarioIndexViewModel()
+            {
+                usuarios = Usuario_Conexion.@select()
+            };
+
+            return View(usuarios);
+        }
+
+        public ActionResult InsertForm()
+        {
+            return View(new Usuario());
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("InsertForm", usuario);
+            }
+
+            int retval = Usuario_Conexion.Insert(usuario);
+            if (ErrorCodes.errorCodes.ContainsKey(retval))
+            {
+                TempData["WarningMessage"] = ErrorCodes.errorCodes[retval];
+                return View("InsertForm", usuario);
+            }
+
+            TempData["SuccessfulMessage"] = "Usuario agregado";
+            return View("InsertForm", new Usuario());
+        }
+
+        [Route("Usuario/Delete/{nombre}")]
+        public void Delete(string nombre)
+        {
+            Usuario_Conexion.Delete(nombre);
+        }
+
+        public ActionResult Detail(string nombre)
+        {
             return View();
         }
     }
---
+++ Project_1/ViewModels/Usuario/UsuarioDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_1.ViewModels.Usuario
+{
+    public class UsuarioDetailViewModel
+    {
+        public Models.Usuario usuario { get; set; }
+        public List<Models.Propiedad> propiedades { get; set; }
+    }
+}
